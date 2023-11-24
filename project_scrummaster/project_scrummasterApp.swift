cd2183e8bd5e2c796a93e98bd5c878dd72c67183
Artur Kubala <ak@arturkubala.com>
2023-11-24 10:12:45 +0000

    Initial Commit

---
+++ project_scrummaster/project_scrummasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_scrummasterApp.swift
+//  project_scrummaster
+//
+//  Created by Artur on 24/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct project_scrummasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
